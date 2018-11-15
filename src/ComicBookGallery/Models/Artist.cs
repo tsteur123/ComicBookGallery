4a8147857616f0d384236acfd427fb3cad7eda70
tsteur123 <tsteur050@gmail.com>
2018-11-15 12:23:14 +0100

    verder

---
+++ src/ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+        public string Role { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+
+        public string Displayt
+        {
+            get { return SeriesTitle + " #" + IssueNumber; }
+        }
+
+        public string CoverImageFile
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-").ToLower()
+                    + "-" + IssueNumber + ".jpg";
+            }
+        }
+
+    }
+}
